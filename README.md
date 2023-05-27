ba1ced1f3b0e7c2c6ca8388e407c385ed3510841
Anna Burdanova <burdanovaas@gmail.com>
2023-05-27 13:31:07 +0700

    feat: :construction: Add Solution For Palindrome Number Problem

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Here are my solutions for the leetcode problems (in js):
 
 - [x] 1. [Two Sums](src/1-two-sum.js). ➡️[Leetcode](https://leetcode.com/problems/two-sum/)
+- [x] 9. [Palindrome Number](src/9-palindrome-number.js). ➡️[Leetcode](https://leetcode.com/problems/palindrome-number/)
--- src/1-two-sum.js
+++ src/1-two-sum.js
@@ -1,4 +1,5 @@
-var twoSum = (nums, target) => {
+// Bruteforce:
+const twoSum = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -14,3 +15,19 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 
 console.log(twoSum(nums, target));
+
+// Using Hashmap (Map in V8 js engine uses "Deterministic hash tables")
+
+const twoSumBetter = (nums, target) => {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [i, map.get(target - nums[i])];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+};
+
+console.log(twoSumBetter(nums, target));
